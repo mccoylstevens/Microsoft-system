52100c3c99047e59b13b67854c5b3deb2835847e
GitHub Action <action@github.com>
2025-05-08 00:10:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/mccoylstevens/microsoftsystem/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/mccoylstevens/microsoftsystem/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.mccoylstevens.microsoftsystem.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mccoylstevens.microsoftsystem.MyBundle
+import com.github.mccoylstevens.microsoftsystem.services.MyProjectService
 import javax.swing.JButton
 
 
